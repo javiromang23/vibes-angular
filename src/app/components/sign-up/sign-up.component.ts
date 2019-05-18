f48cdd0379cda379d90c0218ce59d4a93455e8f6
Javier <javiromang23>
2019-05-18 11:57:32 +0200

    Añadiendo template y funcionalidad en el registro.

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['../sign-in/sign-in.component.css']
 })
 export class SignUpComponent implements OnInit {
+  public user: User;
+  public token: string;
+  public result: boolean;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.token = userService.getToken();
+    this.user = new User('', '', '', '', '', '', new Date(), '', '', '', '');
+  }
 
   ngOnInit() {
+    if (this.token) {
+      this.router.navigate(['/signup']);
+    }
+  }
+
+  onSubmit(form) {
+    this.userService.signUp(this.user).subscribe(
+      response => {
+        this.token = response.token;
+        localStorage.setItem('ssid_session', JSON.stringify(this.token));
+        this.result = true;
+      },
+      error => {
+        this.result = false;
+        console.log(error);
+      }
+    );
   }
 
 }
